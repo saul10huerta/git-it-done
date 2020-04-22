5d305dd6b4d1ca57b0dbc4c25f713c7a598e4da8
Saul Everardo Huerta Perez <Saul@Sauls-MBP.attlocal.net>
2020-04-21 23:40:11 -0500

    added comments

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,6 @@
 var getUserRepos = function() {
     console.log("function was called");
+    //fetch used to gather info from git API
     fetch("https://api.github.com/users/octocat/repos");
 };
   
